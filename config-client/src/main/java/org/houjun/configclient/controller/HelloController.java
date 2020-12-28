24afadd55fefb5f1bd1085bbc75555fd3288c77c
houjun <mrhjgg@gmail.com>
2020-12-28 22:04:05 +0800

    配置中心

---
+++ config-client/src/main/java/org/houjun/configclient/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.houjun.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${javaboy}")
+    String javaboy;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return javaboy;
+    }
+}
---
+++ gateway/src/main/java/org/houjun/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package org.houjun.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+//    @Bean
+//    RouteLocator routeLocator(RouteLocatorBuilder builder){
+//        return builder.routes().route("houjun",r ->r.path("/get").uri("http://httpbin.org")).build();
+//    }
+}
